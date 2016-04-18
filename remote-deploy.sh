88a1634d3bbe9cfd110f1d4befa10a60c98b4447
Domingo Suarez Torres <domingo.suarez@gmail.com>
2016-04-18 09:55:05 -0500

    using deployer user to deploy

--- remote-deploy.sh
+++ remote-deploy.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-./build.sh && rsync -avzhe ssh --delete --delete-after ./public root@104.236.41.205:/www/engineering.holagus.com
+./build.sh && rsync -avzhe ssh --delete --delete-after ./public deployer@104.236.41.205:/www/engineering.holagus.com
